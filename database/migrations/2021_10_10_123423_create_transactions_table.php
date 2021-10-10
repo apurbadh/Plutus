5575665419be09618cae6002c9d04d3788f5cdd8
apurbadh <erikduke182@gmail.com>
2021-10-10 18:24:59 +0545

    transaction migration and model added

---
+++ database/migrations/2021_10_10_123423_create_transactions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger("sender_id");
+            $table->unsignedInteger("reciever_id");
+            $table->integer("amount");
+            $table->timestamps();
+            $table->foreignId("sender_id")
+                  ->references("id")
+                  ->on("users")
+                  ->onDelete("cascade");
+            $table->foreignId("reciever_id")
+                  ->references("id")
+                  ->on("users")
+                  ->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
