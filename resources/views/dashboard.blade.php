e198b632e6c9356cb04d4b4e6d34414464a46fba
apurbadh <erikduke182@gmail.com>
2021-10-10 23:50:39 +0545

    notification completed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,4 +2,7 @@
 
 @section("content")
 
+    <input type="hidden" id="notifications" value="{{ $notifications }}"/>
+    <input type="hidden" id="user_balance" value="{{ Auth::user()->balance }}"/>
+    <div id="dashboard-react"></div>
 @endsection
